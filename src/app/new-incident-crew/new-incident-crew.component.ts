69320a2f6dcc052e9bb04b3287877121fbce3d76
Nikola Nikolic <nikolic.nikola2312@gmail.com>
2021-04-24 13:59:04 +0200

    sredjen  pocetni ekran plus pozadina dodata

---
+++ src/app/new-incident-crew/new-incident-crew.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-incident-crew',
+  templateUrl: './new-incident-crew.component.html',
+  styleUrls: ['./new-incident-crew.component.css']
+})
+export class NewIncidentCrewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
