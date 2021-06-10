54dd2226971442967fe1730283227be19381f807
prica.vesna98@gmail.com <prica.vesna98@gmail.com>
2021-06-10 20:30:43 +0200

    server side for team crud operations pt1

--- WebSERVER/WebSERVER/Controllers/TeamController.cs
+++ WebSERVER/WebSERVER/Controllers/TeamController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,18 +19,48 @@ namespace WebSERVER.Controllers
         {
             _context = context;
         }
+
+        internal static string GetRandomString(int stringLength)
+        {
+            StringBuilder sb = new StringBuilder();
+            int numGuidsToConcat = (((stringLength - 1) / 32) + 1);
+            for (int i = 1; i <= numGuidsToConcat; i++)
+            {
+                sb.Append(Guid.NewGuid().ToString("N"));
+            }
+
+            return sb.ToString(0, stringLength);
+        }
+        [HttpGet]
+        [Route("GenerateTeamID")]
+        public async Task<Object> GenerateTeamID()
+        {
+            string result = GetRandomString(10);
+            return  Ok(result);
+        }
+
         [HttpPost]
         [Route("AddTeam")]
         public async Task<Object> AddTeam(Team team)
         {
             //PREOSTAJE VALIDACIJA NA FRONTU I NA BACKU,MIN 1 CLAN DA POSTOJI I DUZINA STR I BUTTON DISABLE
 
+
             var tim = _context.Teams.FirstOrDefault(x => x.TeamId.Equals(team.TeamId));
             if (tim!=null)
             {
                 //postoji vec tim sa tim idijem     PRIJAVITI
-                var result = "Postoji sa tim id-jem";
-                return Ok(result);  //???duga povratna vrednost
+               var result = "Postoji sa tim id-jem";
+               return Ok(result);  //???duga povratna vrednost
+                //return NotFound();
+            }
+            else if (team.Members.Count() == 0)
+            {
+                //nema nijednig clana ne moze
+                var result = "Mora biti bar jedan clan";
+                //return NotFound();
+                return Ok(result);
+
             }
 
             //ako ne postoji
@@ -51,6 +82,52 @@ namespace WebSERVER.Controllers
                 Console.WriteLine(e.Message);
                 throw e;
             }
+        }
+
+        [HttpGet]
+        [Route("GetAllTeams")]
+        public async Task<ActionResult<IEnumerable<Team>>> GetAllTeams()
+        {
+            //return await _userManager.Users.Where(x => x.Status.ToUpper().Equals("PROCESIRA")).ToListAsync();
+            var timovi = _context.Teams.ToList();
+            var clanovi = _context.Members.ToList();
+
+            for (int i=0;i<timovi.Count;i++)
+            {
+                timovi[i].Members = new List<object>();
+                foreach (var clan in clanovi )
+                {
+                    
+                    if ((clan.MemberTeamId).Equals( timovi[i].TeamId))
+                    {
+                        timovi[i].Members.Add(clan.MemberUserName);
+                    }
+                }
+            }
+
+            return timovi;
+        }
+
+        [HttpPost]
+        [Route("DeleteTeam")]
+        public async Task<ActionResult> DeleteTeam([FromForm] string teamId)
+        {
+            var obrisiTim = _context.Teams.Where(x => x.TeamId.Equals(teamId)).Single();
+            _context.Teams.Remove(obrisiTim);
+
+            //obrisati i sve clanove
+            foreach(var clan in _context.Members.ToList())
+            {
+                if (clan.MemberTeamId.Equals(teamId))
+                {
+                    _context.Members.Remove(clan);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            var result = "Uspesno obrisan tim i njegovi clanovi";
+            return Ok(result);
         }
     }
 }
