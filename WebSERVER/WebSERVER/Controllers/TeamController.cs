ce8d03d3d54648d0e1bf57285be2de63a0536253
prica.vesna98@gmail.com <prica.vesna98@gmail.com>
2021-06-13 15:46:19 +0200

    crud for team pt2

--- WebSERVER/WebSERVER/Controllers/TeamController.cs
+++ WebSERVER/WebSERVER/Controllers/TeamController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebSERVER.Models;
 
@@ -14,10 +15,11 @@ namespace WebSERVER.Controllers
     public class TeamController : ControllerBase
     {
         private readonly WebServerContext _context;
-
-        public TeamController(WebServerContext context)
+        private UserManager<ApplicationUser> _userManager;
+        public TeamController(WebServerContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         internal static string GetRandomString(int stringLength)
@@ -37,6 +39,69 @@ namespace WebSERVER.Controllers
         {
             string result = GetRandomString(10);
             return  Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GetTeam")]
+        public async Task<ActionResult<Team>> GetTeam([FromForm] string timId)
+        {
+             var tim = _context.Teams.FirstOrDefault(x => x.TeamId.Equals(timId));//nastaviti
+            //tim.Members = new List<object>();
+            //foreach(var clan in _context.Members.ToList())
+            //{
+            //    if (clan.MemberTeamId.Equals(timId))
+            //    {
+            //        tim.Members.Add(clan.MemberUserName);
+            //    }
+            //}
+           return tim;
+           //return Ok("poruka");
+        }
+        [HttpPost]
+        [Route("GetMembers")]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetMembers([FromForm] string timId)
+        {
+            List<ApplicationUser> members = new List<ApplicationUser>();
+           
+            foreach (var clan in _context.Members.ToList())
+            {
+                if (clan.MemberTeamId.Equals(timId))
+                {
+                    //treba naci sa tim usernamom
+                    members.Add(await _userManager.FindByNameAsync(clan.MemberUserName));
+                }
+            }
+            return members;
+        }
+
+        [HttpPost]
+        [Route("GetFreeMembers")]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetFreeMembers([FromForm] string timId)
+        {
+            List<ApplicationUser> freeMembers = new List<ApplicationUser>();
+            List<MemberOfTeam> members = new List<MemberOfTeam>();//koji su vec clanovi
+
+            foreach (var clan in _context.Members.ToList())
+            {
+                if (clan.MemberTeamId.Equals(timId))
+                {
+                    //treba naci sa tim usernamom
+                    members.Add(clan);
+                }
+            }
+
+            foreach (var user in _userManager.Users.ToList())
+            {
+                if(user.Status.Equals("prihvacen") && user.UserRole.ToUpper().Equals("CLAN EKIPE"))
+                {
+                    var temp = members.FirstOrDefault(x => x.MemberUserName.Equals(user.UserName));//ako nije null postoji, ako je null FREE JE
+                    if (temp == null)
+                    {
+                        freeMembers.Add(user);
+                    }
+                }
+            }
+            return freeMembers;
         }
 
         [HttpPost]
@@ -78,6 +143,60 @@ namespace WebSERVER.Controllers
                 return Ok(result);
             }
             catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        [Route("UpdateTeam")]
+        public async Task<Object> UpdateTeam(Team team)
+        {
+            var tim = _context.Teams.FirstOrDefault(x => x.TeamId.Equals(team.TeamId));
+            if (tim == null)
+            {
+                //obrisam tim sa tim idijem     PRIJAVITI
+                var result = "Ne postoji tim sa tim id-jem";
+                return Ok(result); 
+                //return NotFound();
+            }
+            else if (team.Members.Count() == 0)
+            {
+                //nema nijednig clana ne moze
+                var result = "Mora biti bar jedan clan";
+                return Ok(result);
+
+            }
+
+            try
+            {
+                foreach (var clan in _context.Members.ToList())
+                {
+                    if (clan.MemberTeamId.Equals(tim.TeamId) && !team.Members.Contains(clan.MemberUserName))//obrisi onog koji nije vise u timu
+                    {
+                        //
+                        _context.Members.Remove(clan);
+                    }else if(clan.MemberTeamId.Equals(tim.TeamId) && team.Members.Contains(clan.MemberUserName))
+                    {
+                        team.Members.Remove(clan.MemberUserName);//obrisi iz liste da bi normalno prolazio i dodao potrebne
+                    }
+                }
+
+                var clanovi = team.Members;
+                foreach (var clan in clanovi)
+                {
+                    Console.WriteLine(clan);
+                    _context.Members.Add(new MemberOfTeam { MemberTeamId = tim.TeamId, MemberUserName = clan as string });
+                }
+                //_context.Teams.Update(team);
+                tim.TeamName = team.TeamName;
+
+                await _context.SaveChangesAsync();
+                var result = "Uspesno izmenjen tim";
+                return Ok(result);
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 throw e;
